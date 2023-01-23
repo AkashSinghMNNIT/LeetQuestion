7c1839944351418b719d83a2c035018ef933957e
AkashSinghMNNIT <92155503+AkashSinghMNNIT@users.noreply.github.com>
2023-01-23 11:17:56 +0530

    Time: 357 ms (11.37%), Space: 65.7 MB (24.16%) - LeetHub

---
+++ 0491-non-decreasing-subsequences/0491-non-decreasing-subsequences.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    
+    vector<int> fun(int i, vector<int> &nums)
+    {
+        if(__builtin_popcount(i)<2) return {};
+        int j = 0;
+        vector<int> v;
+        while(i)
+        {
+            if(i&1) 
+            {
+                if(v.empty() || v.back()<=nums[j])
+                v.push_back(nums[j]);
+                else return {};
+            }
+            i/=2;
+            ++j;
+        }
+        return v;
+    }
+    
+    vector<vector<int>> findSubsequences(vector<int>& nums) {
+        int n = nums.size();
+        vector<vector<int>> ans;
+        map<vector<int>, int> mp;
+        int en = (1<<n) - 1;
+        for(int i = 3; i<=en; ++i)
+        {
+            vector<int> v = fun(i, nums);
+            if(!v.empty()) mp[v]= 1;
+        }
+        for(auto [v, flag]: mp)ans.push_back(v);
+        
+        return ans;
+    }
+};
