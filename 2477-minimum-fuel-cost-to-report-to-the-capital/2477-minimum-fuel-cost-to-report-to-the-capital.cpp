6afd2962f9d3d00f7c40dd510ec12b65659d1de6
AkashSinghMNNIT <92155503+AkashSinghMNNIT@users.noreply.github.com>
2023-02-12 15:55:49 +0530

    Time: 718 ms (45.57%), Space: 200.9 MB (51.86%) - LeetHub

--- 2477-minimum-fuel-cost-to-report-to-the-capital/2477-minimum-fuel-cost-to-report-to-the-capital.cpp
+++ 2477-minimum-fuel-cost-to-report-to-the-capital/2477-minimum-fuel-cost-to-report-to-the-capital.cpp
@@ -1,29 +1,35 @@
 class Solution {
 public:
-    
-    long long dfs(int pre, int i, int seat, int depth, long long &ans, vector<int> adj[])
+    // vector<int> vis;
+    int vis[100002];
+    int fun(vector<int> adj[], int idx, long long &ans, int seat)
     {
-        long long rider = 1;
-        for(auto it: adj[i])
+        if(vis[idx]) return 0;
+        vis[idx] = 1;
+        int rider = 1;
+        for(auto i: adj[idx])
         {
-            if(it == pre) continue;
-            rider += dfs(i, it, seat, depth+1, ans, adj);
+            int x = fun(adj, i, ans, seat);
+            ans += x/seat + (x%seat>0);
+            rider += x;
         }
-        if(depth == 0) return ans;
-        ans += (rider/seat)*depth + (rider%seat ? 1 : 0);
-        
-        return rider%seat;
+        return rider;
     }
     
     long long minimumFuelCost(vector<vector<int>>& roads, int seats) {
-        
-        vector<int> adj[roads.size()+1];
+        int n = roads.size();
+        // vis.resize(n);
+        // fill(vis.begin(), vis.end(), 0);
+        // memset(vis, 0, sizeof(vis));
+        long long ans = 0, cnt = 0, dis = 0;
+        vector<int> adj[n+1];
         for(auto it: roads)
         {
             adj[it[0]].push_back(it[1]);
             adj[it[1]].push_back(it[0]);
         }
-        long long ans = 0;
-        return dfs(-1, 0, seats, 0, ans, adj);
+        
+        fun(adj, 0, ans, seats);
+        return ans;
     }
 };
