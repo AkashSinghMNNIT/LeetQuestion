70ce9a54d8cb621ad2a85f31f9b615b69cad2021
AkashSinghMNNIT <92155503+AkashSinghMNNIT@users.noreply.github.com>
2023-03-26 18:48:32 +0530

    Added solution - LeetHub

--- Longest alternating subsequence - GFG/longest-alternating-subsequence.cpp
+++ Longest alternating subsequence - GFG/longest-alternating-subsequence.cpp
@@ -6,37 +6,14 @@ using namespace std;
 class Solution {
 	public:
 		int AlternatingaMaxLength(vector<int>&nums){
-		    stack<int> gr, sm;
-		    int n = nums.size(), ans = 1;
-		    int pgr[n], psm[n];
-		    for(int i = 0; i<n; ++i)
+		    int mn = 1, mx = 1, n = nums.size();
+		    for(int i = 1; i<n; ++i)
 		    {
-		        while(!gr.empty() && nums[gr.top()] <= nums[i]) gr.pop();
-		        if(gr.empty()) pgr[i] = -1;
-		        else pgr[i] = gr.top();
-		        
-		        gr.push(i);
-		        
-		        while(!sm.empty() && nums[sm.top()] >= nums[i]) sm.pop();
-		        if(sm.empty()) psm[i] = -1;
-		        else psm[i] = sm.top();
-		        
-		        sm.push(i);
-		    }
-		    
-		    vector<vector<int>> dp(n, vector<int>(2, 0));
-		    
-		    for(int i = 0; i<n; ++i)
-		    {
-		        if(psm[i]!=-1) dp[i][0] = max(1+dp[psm[i]][1], dp[psm[i]][0]);
-		        else dp[i][0] = 1;
-		        
-		        if(pgr[i]!=-1) dp[i][1] = max(1+dp[pgr[i]][0], dp[pgr[i]][1]);
-		        else dp[i][1] = 1;
-		        ans = max({ans, dp[i][0], dp[i][1]});
+		        if(nums[i] > nums[i-1]) mx = 1 + mn;
+		        else if(nums[i] < nums[i-1]) mn = 1+mx;
 		    }
 		  
-		    return ans;
+		    return max(mn, mx);
 		}
 		
 
