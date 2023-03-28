c5890ae9e126b765c4f9344ea2bcdbcaa5a78a72
AkashSinghMNNIT <92155503+AkashSinghMNNIT@users.noreply.github.com>
2023-03-28 18:00:33 +0530

    Added solution - LeetHub

---
+++ Shy Geek - GFG/shy-geek.cpp
@@ -0,0 +1,100 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+
+#include<bits/stdc++.h>
+using namespace std;
+
+class Shop
+{
+    vector<int> chocolates;
+    int countOfCalls;
+    public:
+    Shop()
+    {
+        countOfCalls = 0;
+    }
+    void addChocolate(int x)
+    {
+        chocolates.push_back(x);
+    }
+    long long get(int i)
+    {
+        countOfCalls++;
+        if(i<0 || i>=(int)chocolates.size() || countOfCalls>50)return -1;
+        return chocolates[i];
+    }
+};
+
+
+// } Driver Code Ends
+//User function Template for Java
+
+/*
+Instructions - 
+
+    1. 'shop' is object of class Shop.
+    2. User 'shop.get(int i)' to enquire about the price
+            of the i^th chocolate.
+    3. Every chocolate in shop is arranged in increasing order
+            i.e. shop.get(i) <= shop.get(i + 1) for all 0 <= i < n - 1
+*/
+class Solution{
+    public:
+    
+    Shop shop;
+    Solution(Shop s)
+    {
+        this->shop = s;
+    }
+    long long find(int n, long k){
+        int x, l = -1, h = n;
+        
+        while(h-l>1)
+        {
+            int mid = (h+l)/2;
+            x = shop.get(mid);
+            if(x == -1) return 2;
+            if(x <= k) l = mid;
+            else h =mid;
+        }
+        if(l == -1) return 0;
+        else  
+        {
+            x = shop.get(l);
+            if(x == -1) return 2;
+            return k/x + find(l+2, k%x);
+        }
+    }
+
+};
+
+//{ Driver Code Starts.
+
+int main(){
+    int t;
+    scanf("%d ",&t);
+    while(t--){
+        
+        int n;
+        scanf("%d",&n);
+        long long k;
+        cin >> k;
+        
+        Shop shop;
+        for(int i = 0; i<n; i++)
+        {
+            int x;
+            cin >> x;
+            shop.addChocolate(x);
+        }
+        
+        Solution obj(shop);
+        long long res = obj.find(n, k);
+        
+        cout<<res<<endl;
+        
+    }
+}
+
+// } Driver Code Ends
