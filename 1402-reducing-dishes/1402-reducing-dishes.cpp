cdcca33c67cc3dd650fd7a8837e7f365f84145a4
AkashSinghMNNIT <92155503+AkashSinghMNNIT@users.noreply.github.com>
2023-03-29 18:44:47 +0530

    Time: 33 ms (53.67%), Space: 20.9 MB (49.34%) - LeetHub

---
+++ 1402-reducing-dishes/1402-reducing-dishes.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int maxSatisfaction(vector<int>& v) {
+        int n = size(v), inf = 1e8;
+        sort(begin(v), end(v));
+        vector<vector<int>> dp(n+1, vector<int>(n+1, -inf));
+        for(int i=0; i<=n; ++i) dp[i][0] = 0;
+        for(int i = 1; i<=n; ++i)
+        {
+            for(int j = 1; j<=i; ++j)
+            {
+                dp[i][j] = max(dp[i-1][j], v[i-1]*j + dp[i-1][j-1]);
+            }
+        }
+        int ans = 0;
+        for(int i = 0; i<=n; ++i) ans = max(ans, dp[n][i]);
+        return ans;
+    }
+};
