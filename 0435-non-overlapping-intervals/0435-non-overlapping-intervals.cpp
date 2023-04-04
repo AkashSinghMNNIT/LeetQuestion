a48e924c5b686008904c133831a792bec4fac174
AkashSinghMNNIT <92155503+AkashSinghMNNIT@users.noreply.github.com>
2023-04-04 17:28:17 +0530

    Time: 521 ms (26.03%), Space: 89.8 MB (88.09%) - LeetHub

--- 0435-non-overlapping-intervals/0435-non-overlapping-intervals.cpp
+++ 0435-non-overlapping-intervals/0435-non-overlapping-intervals.cpp
@@ -1,20 +1,22 @@
-class Solution {
-public:
-    
-    static bool cmp(vector<int> &a, vector<int> &b)
-    {
-        return (a[1]==b[1]? a[0]<b[0]:a[1]<b[1]);
-    }
-    
-    int eraseOverlapIntervals(vector<vector<int>>& a) {
-        int n = size(a), ans = 0, l = -1e6;
-        sort(begin(a), end(a), cmp);
-        
-        for(int i = 0; i<n; ++i)
+class Solution
+{
+    public:
+        static bool cmp(vector<int> &a, vector<int> &b)
         {
-            if(a[i][0] < l) ++ans;
-            else l = a[i][1];
+            return (a[1] == b[1] ? a[0]<b[0]: a[1]<b[1]);
         }
-        return ans;
+    int eraseOverlapIntervals(vector<vector < int>> &intervals)
+    {
+        sort(intervals.begin(), intervals.end(), cmp);
+        int temp = 1, l = intervals[0][1], n = intervals.size();
+        for (int i = 1; i < intervals.size(); i++)
+        {
+            if (intervals[i][0] >= l)
+            {
+                temp++;
+                l = intervals[i][1];
+            }
+        }
+        return (n - temp);
     }
 };
