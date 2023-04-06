ccb1e86ffdeca8333710e6059689044822a69180
AkashSinghMNNIT <92155503+AkashSinghMNNIT@users.noreply.github.com>
2023-04-06 16:48:48 +0530

    Time: 73 ms (69.70%), Space: 23.1 MB (41.03%) - LeetHub

--- 0646-maximum-length-of-pair-chain/0646-maximum-length-of-pair-chain.cpp
+++ 0646-maximum-length-of-pair-chain/0646-maximum-length-of-pair-chain.cpp
@@ -1,22 +1,25 @@
-class Solution {
-public:
-    
-    static bool cmp(vector<int> &a, vector<int> &b)
+class Solution
+{
+    public:
+
+        static bool cmp(vector<int> &a, vector<int> &b)
+        {
+            return (a[1] == b[1] ? a[0] < b[0] : a[1] < b[1]);
+        }
+
+    int findLongestChain(vector<vector < int>> &p)
     {
-        return (a[1]==b[1]?a[0]<b[0]:a[1]<b[1]);
-    }
-    
-    int findLongestChain(vector<vector<int>>& p) {
         int n = size(p);
         sort(begin(p), end(p), cmp);
-        // vector<int> temp(n);
-        // for(int i = 0; i<n; ++i) temp[i] = p[i][1];
-        int l = -1e4, ans = 0;
-        for(auto it: p)
+        vector<int> temp(n), dp(n, 1);
+        for (int i = 0; i < n; ++i) temp[i] = p[i][1];
+
+        for (int i = 0; i < n; ++i)
         {
-            if(it[0] > l) ++ans, l = it[1];
+            int j = lower_bound(begin(temp), end(temp), p[i][0]) - begin(temp) - 1;
+            dp[i] = max((i>0?dp[i - 1]:1), 1 + (j >= 0 ? dp[j] : 0));
         }
-        cout<<endl;
-        return ans;
+
+        return dp[n - 1];
     }
 };
