cf0c96725d857d4c75fd782db24f8a2ff785c80f
AkashSinghMNNIT <92155503+AkashSinghMNNIT@users.noreply.github.com>
2026-04-19 15:55:18 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1471-the-k-strongest-values-in-an-array/1471-the-k-strongest-values-in-an-array.cpp
+++ 1471-the-k-strongest-values-in-an-array/1471-the-k-strongest-values-in-an-array.cpp
@@ -1,17 +1,18 @@
 class Solution {
 public:
     vector<int> getStrongest(vector<int>& arr, int k) {
-        vector<pair<int, int>> v;
+        priority_queue<pair<int, int>, vector<pair<int, int>>> q;
         sort(arr.begin(), arr.end());
         int n = arr.size();
         int mid = arr[(n-1)/2];
         for(auto it: arr){
-            v.push_back({abs(it-mid), it});
+            q.push({abs(it-mid), it});
         }
-        sort(v.begin(), v.end());
         vector<int> ans;
-        for(int i = n-1; i>n-1-k; --i){
-            ans.push_back(v[i].second);
+        while(k--){
+            auto top = q.top();
+            q.pop();
+            ans.push_back(top.second);
         }
         return ans;
     }
