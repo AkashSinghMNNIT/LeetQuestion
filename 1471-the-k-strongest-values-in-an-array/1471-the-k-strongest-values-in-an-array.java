f316d6f32f9448057e27db5ecc20a90821d9ae04
AkashSinghMNNIT <92155503+AkashSinghMNNIT@users.noreply.github.com>
2026-04-19 16:03:11 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1471-the-k-strongest-values-in-an-array/1471-the-k-strongest-values-in-an-array.java
+++ 1471-the-k-strongest-values-in-an-array/1471-the-k-strongest-values-in-an-array.java
@@ -3,17 +3,15 @@ class Solution {
         Arrays.sort(arr);
         int n = arr.length;
         int mid = arr[(n-1)/2];
-        int[][] v = new int[n][2];
-        for(int i = 0; i<n; ++i){
-            v[i][0] = Math.abs(arr[i]-mid);
-            v[i][1] = arr[i];
-        }
-        Arrays.sort(v, (a, b) -> {
-            if(a[0] == b[0]) return b[1]-a[1];
-            else return b[0]-a[0];
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a, b)->{
+            if(a[0]==b[0]) return b[1]-a[1];
+            return b[0]-a[0];
         });
+        for(int i: arr){
+            pq.offer(new int[]{Math.abs(i-mid), i});
+        }
         int[] ans = new int[k];
-        for(int i = 0; i<k; ++i) ans[i] = v[i][1];
+        for(int i = 0; i<k; ++i) ans[i] = pq.poll()[1];
         return ans;
     }
 }
